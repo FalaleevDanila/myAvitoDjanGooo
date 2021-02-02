1ef67cd5622e0221a8154ef7a403e52d2a40b0b4
FalaleevDanila <FalaleevDanila@gmail.ru>
2021-02-02 03:31:43 +0300

    start

---
+++ bboard/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Bb
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content',)
+
+
+admin.site.register(Bb, BbAdmin)
+# Register your models here.
