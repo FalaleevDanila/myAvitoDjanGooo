416e6ea2c31e92f2c15087c9908ec25c68b87ca3
FalaleevDanila <FalaleevDanila@gmail.ru>
2021-02-05 01:52:54 +0300

    + update urls + add ADD + some css

--- bboard/views.py
+++ bboard/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
 
 
 def index(request):
     bbs = Bb.objects.all()
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
 
+
+from django.views.generic.edit import CreateView
+from .form import BbForm
+from django.urls import reverse_lazy
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
